9ea7af1b59375b2f9e3384c583ebed738bcdc8d7
Shahmeer Khan <shahmeerkh2006@gmail.com>
2025-11-12 23:53:48 -0500

    Implemented base version of CollectionInteractor for creating a new collection and adding initial countries to it, and implemented CollectionOutputBoundary

--- src/main/java/use_case/collection/CollectionInputBoundary.java
+++ src/main/java/use_case/collection/CollectionInputBoundary.java
@@ -1,4 +1,12 @@
 package use_case.collection;
 
+/**
+ * The Collection Use Case.
+ */
 public interface CollectionInputBoundary {
+    /**
+     * Execute the Collection Use Case.
+     * @param collectionInputData the input data for this use case
+     */
+    void execute(CollectionInputData collectionInputData);
 }
