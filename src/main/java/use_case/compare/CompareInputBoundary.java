c42766aa769b794da8ea0fb705a61bf60c9e2d02
Neel Patel <npatell0126@gmail.com>
2025-11-17 20:15:18 -0500

    Added input and output boundaries for Compare use case

--- src/main/java/use_case/compare/CompareInputBoundary.java
+++ src/main/java/use_case/compare/CompareInputBoundary.java
@@ -1,4 +1,9 @@
 package use_case.compare;
 
 public interface CompareInputBoundary {
+
+    /**
+     * Execute the Compare use case with the given input data.
+     */
+    void execute(CompareInputData inputData);
 }
--- src/main/java/use_case/compare/CompareOutputBoundary.java
+++ src/main/java/use_case/compare/CompareOutputBoundary.java
@@ -1,4 +1,14 @@
 package use_case.compare;
 
 public interface CompareOutputBoundary {
+
+    /**
+     * Called when the comparison succeeds.
+     */
+    void prepareSuccessView(CompareOutputData outputData);
+
+    /**
+     * Called when the comparison fails (invalid selection, missing data, etc.).
+     */
+    void prepareFailView(String errorMessage);
 }
