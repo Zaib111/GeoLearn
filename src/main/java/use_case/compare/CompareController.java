38afe3790a6f4b1b3eeb12dc963aaee940faca1c
Neel Patel <npatell0126@gmail.com>
2025-11-17 20:33:43 -0500

    Added CompareController to call Compare use case

--- src/main/java/use_case/compare/CompareController.java
+++ src/main/java/use_case/compare/CompareController.java
@@ -1,4 +1,23 @@
 package use_case.compare;
 
+import use_case.compare.CompareInputBoundary;
+import use_case.compare.CompareInputData;
+
+import java.util.List;
+
 public class CompareController {
+
+    private final CompareInputBoundary interactor;
+
+    public CompareController(CompareInputBoundary interactor) {
+        this.interactor = interactor;
+    }
+
+    /**
+     * Called by the view when the user clicks "Compare".
+     */
+    public void compare(List<String> countryCodes) {
+        CompareInputData inputData = new CompareInputData(countryCodes);
+        interactor.execute(inputData);
+    }
 }
