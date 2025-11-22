3faba94c672f94f1deaa71da3a8e2b0cd90cd368
Shahmeer Khan <shahmeerkh2006@gmail.com>
2025-11-22 13:13:24 -0500

    Implemented delete, rename and edit collection features for collection use case (alternate flows), enhanced display with flags and country stats from api, and validation

--- src/main/java/app/use_cases/collection/CollectionUserDataAccessInterface.java
+++ src/main/java/app/use_cases/collection/CollectionUserDataAccessInterface.java
@@ -3,8 +3,13 @@ package app.use_cases.collection;
 import app.entities.CountryCollection;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 public interface CollectionUserDataAccessInterface {
     void createCollection(CountryCollection countryCollection);
     List<CountryCollection> getAllCollections();
+    Optional<CountryCollection> getCollectionById(UUID collectionId);
+    void deleteCollection(UUID collectionId);
+    void updateCollection(CountryCollection updatedCollection);
 }
