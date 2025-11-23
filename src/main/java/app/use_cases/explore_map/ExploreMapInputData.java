fa31d99540476ce30aadb571572b94f796497d08
Neel Patel <npatell0126@gmail.com>
2025-11-23 12:50:07 -0500

    Added comments

--- src/main/java/app/use_cases/explore_map/ExploreMapInputData.java
+++ src/main/java/app/use_cases/explore_map/ExploreMapInputData.java
@@ -1,15 +1,28 @@
 package app.use_cases.explore_map;
 
 /**
- * Input data for loading a map.
+ * Input data for requesting a map to be loaded in the Explore Map use case.
+ * Contains the file system path to the shapefile that should be loaded.
  */
 public class ExploreMapInputData {
+
+    /** The path to the shapefile containing geographical data. */
     private final String shapefilePath;
 
+    /**
+     * Creates a new input data object for a map loading request.
+     *
+     * @param shapefilePath the file path to the shapefile to load
+     */
     public ExploreMapInputData(String shapefilePath) {
         this.shapefilePath = shapefilePath;
     }
 
+    /**
+     * Gets the shapefile path associated with the map load request.
+     *
+     * @return the shapefile file path as a string
+     */
     public String getShapefilePath() {
         return shapefilePath;
     }
