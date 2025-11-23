3b5d2d04ee805f1350f2f82ec0a8ca9ddfbc6481
Neel Patel <npatell0126@gmail.com>
2025-11-23 13:04:27 -0500

    Added comments to the file

--- src/main/java/app/use_cases/explore_map/ExploreMapModeInputData.java
+++ src/main/java/app/use_cases/explore_map/ExploreMapModeInputData.java
@@ -4,16 +4,31 @@ package app.use_cases.explore_map;
  * Input data for changing the interaction mode.
  */
 public class ExploreMapModeInputData {
+
+    /**
+     * Supported user interaction modes.
+     */
     public enum InteractionMode {
-        PAN, ZOOM, SELECT
+        PAN,
+        ZOOM,
+        SELECT
     }
 
+    /** The chosen interaction mode. */
     private final InteractionMode mode;
 
+    /**
+     * Create input data with a selected interaction mode.
+     *
+     * @param mode the new user interaction mode
+     */
     public ExploreMapModeInputData(InteractionMode mode) {
         this.mode = mode;
     }
 
+    /**
+     * @return the selected interaction mode
+     */
     public InteractionMode getMode() {
         return mode;
     }
