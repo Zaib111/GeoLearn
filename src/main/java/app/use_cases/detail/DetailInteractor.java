14a5190390e1d2790f98bfade9c5fe501c8ec38b
edricz <edric09821@gmail.com>
2025-11-23 12:13:13 -0500

    Completed setup for the preparing the Detail view

--- src/main/java/app/use_cases/detail/DetailInteractor.java
+++ src/main/java/app/use_cases/detail/DetailInteractor.java
@@ -2,21 +2,23 @@ package app.use_cases.detail;
 import app.use_cases.country.CountryDataAccessInterface;
 
 public class DetailInteractor implements DetailInputBoundary{
-    private final CountryDataAccessInterface dataAccess;
+    private final DetailDataAccessInterface dataAccess;
     private final DetailOutputBoundary output;
-    public DetailInteractor(CountryDataAccessInterface dataAccess, DetailOutputBoundary output) {
+    public DetailInteractor(DetailDataAccessInterface dataAccess, DetailOutputBoundary output) {
         this.dataAccess = dataAccess;
         this.output = output;
     }
     @Override
     public void execute(DetailInputData detailInputData){
-        final String countryCode = detailInputData.getCountryCode();
-        if(!dataAccess.getCountries().contains(countryCode)){
+        final String countryName = detailInputData.getCountryName();
+        /*if(!dataAccess.getCountries().contains(countryCode)){
             output.prepareFailureView("Country Does Not Exist");
         }
         else{
-            DetailOutputData outputData = new DetailOutputData(dataAccess.getCountry(countryCode));
-            output.prepareSuccessView(outputData);
-        }
+            DetailOutputData outputData = new DetailOutputData(dataAccess.getCountryByCode(countryCode));
+            output.prepareDetailView(outputData);
+        }*/
+        DetailOutputData outputData = new DetailOutputData(dataAccess.getCountryByName(countryName));
+        output.prepareDetailView(outputData);
     }
 }
