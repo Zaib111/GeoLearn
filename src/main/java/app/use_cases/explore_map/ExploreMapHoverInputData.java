b6bbed58bd3b85c048dae1e1db4d55974df72a9b
Neel Patel <npatell0126@gmail.com>
2025-11-23 12:48:50 -0500

    Added comments

--- src/main/java/app/use_cases/explore_map/ExploreMapHoverInputData.java
+++ src/main/java/app/use_cases/explore_map/ExploreMapHoverInputData.java
@@ -1,21 +1,42 @@
 package app.use_cases.explore_map;
 
 /**
- * Input data for hovering over a feature on the map.
+ * Input data for a hover interaction on the map.
+ * Represents the on-screen position of the cursor when the user hovers.
  */
 public class ExploreMapHoverInputData {
+
+    /** The x-coordinate of the cursor on the screen. */
     private final double screenX;
+
+    /** The y-coordinate of the cursor on the screen. */
     private final double screenY;
 
+    /**
+     * Creates a new input data object for a hover event.
+     *
+     * @param screenX the x-position of the cursor on the screen
+     * @param screenY the y-position of the cursor on the screen
+     */
     public ExploreMapHoverInputData(double screenX, double screenY) {
         this.screenX = screenX;
         this.screenY = screenY;
     }
 
+    /**
+     * Gets the screen x-coordinate of the cursor.
+     *
+     * @return the x position in pixels
+     */
     public double getScreenX() {
         return screenX;
     }
 
+    /**
+     * Gets the screen y-coordinate of the cursor.
+     *
+     * @return the y position in pixels
+     */
     public double getScreenY() {
         return screenY;
     }
