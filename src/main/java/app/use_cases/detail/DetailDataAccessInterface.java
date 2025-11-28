01ebe576a99ea21b77dea222d88a3868fcb96f06
Marcus Chuong <marcus.chuong@mail.utoronto.ca>
2025-11-28 16:22:59 -0500

    fixed checkstyle issues

--- src/main/java/app/use_cases/detail/DetailDataAccessInterface.java
+++ src/main/java/app/use_cases/detail/DetailDataAccessInterface.java
@@ -4,9 +4,22 @@ import app.entities.Country;
 
 import java.util.List;
 
+/**
+ * Data access interface for detail use case.
+ */
 public interface DetailDataAccessInterface {
 
+    /**
+     * Get a country by its code.
+     * @param code the country code
+     * @return the country
+     */
     Country getCountryByCode(String code);
+
+    /**
+     * Get all countries.
+     * @return list of all countries
+     */
     List<Country> getCountries();
 
 }
--- src/main/java/app/use_cases/detail/DetailInteractor.java
+++ src/main/java/app/use_cases/detail/DetailInteractor.java
@@ -1,18 +1,30 @@
 package app.use_cases.detail;
+
 import app.entities.Country;
 
-public class DetailInteractor implements DetailInputBoundary{
+/**
+ * Interactor for detail use case.
+ */
+public class DetailInteractor implements DetailInputBoundary {
     private final DetailDataAccessInterface dataAccess;
     private final DetailOutputBoundary output;
-    public DetailInteractor(DetailDataAccessInterface dataAccess, DetailOutputBoundary output) {
+
+    /**
+     * Constructor.
+     * @param dataAccess the data access interface
+     * @param output the output boundary
+     */
+    public DetailInteractor(final DetailDataAccessInterface dataAccess,
+                           final DetailOutputBoundary output) {
         this.dataAccess = dataAccess;
         this.output = output;
     }
+
     @Override
-    public void execute(DetailInputData detailInputData){
+    public void execute(final DetailInputData detailInputData) {
         final String countryCode = detailInputData.getCountryCode();
         final Country country = dataAccess.getCountryByCode(countryCode);
-        DetailOutputData outputData = new DetailOutputData(country);
+        final DetailOutputData outputData = new DetailOutputData(country);
         output.prepareDetailSuccessView(outputData);
     }
 }
