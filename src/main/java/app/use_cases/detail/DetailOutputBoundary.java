2371860c0ca9d66d85288d38980d7ca5a2c94f2a
Marcus Chuong <49174541+Marcus-Chuong@users.noreply.github.com>
2025-11-30 13:18:53 -0500

    Revert "Detail5 fixes"

--- src/main/java/app/use_cases/detail/DetailOutputBoundary.java
+++ src/main/java/app/use_cases/detail/DetailOutputBoundary.java
@@ -1,19 +1,7 @@
 package app.use_cases.detail;
 
-/**
- * Output boundary for detail use case.
- */
 public interface DetailOutputBoundary {
 
-    /**
-     * Prepare the failure view.
-     * @param errorMessage the error message
-     */
     void prepareDetailFailureView(String errorMessage);
-
-    /**
-     * Prepare the success view.
-     * @param detailOutputData the output data
-     */
     void prepareDetailSuccessView(DetailOutputData detailOutputData);
 }
